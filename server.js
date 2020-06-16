b7e68e69f1ad2b35ab16ff8b8904cb12b858d0b4
kimmiesjensen <kimm0961@videndjurs.net>
2020-06-16 11:52:52 +0200

    k

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 // require('dotenv').config()
 
-const express = require('express')
-var session = require('express-session')
 var cors = require('cors')
+const express = require('express')
 const app = express()
 
+// Mongoose og DB
+const mongoose = require('mongoose')
+
+// // Localhost-databasen:
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// Atlas-databasen (remote)
+mongoose.connect('mongodb+srv://kimm0961:BofOpgor45@cluster0-rxmwo.azure.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+
+
+// App use
+app.use(cors({ credentials: true, origin: true }));
+app.use(express.static('public')); // statiske filer - upload billder til backend
+app.use(express.json()) // nødevndig når post data er i json
+app.use(express.urlencoded({extended: true})) // ellers er req.body undefined eller tom
+
 app.set('trust proxy', 1);
+
+const session = require('express-session');
 
 // for at bruge mongo som store for session - isf hukommelsen som ikke dur på fx heroku
 const MongoStore = require('connect-mongo')(session);
@@ -23,45 +45,26 @@ const {
 
 const IN_PROD = NODE_ENV === 'production'
 
-// Mongoose og DB
-const mongoose = require('mongoose')
 
-// // Localhost-databasen:
-// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-
-// Atlas-databasen (remote)
-mongoose.connect('mongodb+srv://kimm0961:BofOpgor45@cluster0-rxmwo.azure.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
-
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-
-// App use
-
-app.use(cors()); // HUSK DENNE
-app.use('/public',express.static('public')); // statiske filer - upload billder til backend
-app.use(express.json()) // nødevndig når post data er i json
-app.use(express.urlencoded({extended: true})) // ellers er req.body undefined eller tom
 
 // Session
 app.use(session({
     name: SESS_NAME,
-    resave: false,
+    resave: true,
     saveUninitialized: false,
     store: new MongoStore({mongooseConnection: db}),
     secret: SESS_SECRET,
     cookie: {
         maxAge: TWO_HOURS,
         sameSite: "none",
-        secure: IN_PROD
+        secure: IN_PROD,
+        httpOnly: true
     }
 }))
 
 //route indeholder ordet admin
 
-app.use('*/admin', (req, res, next) => {
+app.use('*/admin*', (req, res, next) => {
 
     // hvis der ikke er en session
     if (!req.session.userId) {
