c18705b592310c9935d38621d95d6650f56cf049
kimmiesjensen <kimm0961@videndjurs.net>
2020-06-16 09:05:13 +0200

    app.use

--- server.js
+++ server.js
@@ -29,11 +29,11 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
-expressApp.use(cors({ credentials: true, origin: "http://localhost:3001/admin" }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 // App use
 
-app.use(cors()); // HUSK DENNE
+// app.use(cors()); // HUSK DENNE
 app.use('/public',express.static('public')); // statiske filer - upload billder til backend
 app.use(express.json()) // nødevndig når post data er i json
 app.use(express.urlencoded({extended: true})) // ellers er req.body undefined eller tom
